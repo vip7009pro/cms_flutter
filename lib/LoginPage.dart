1df715574ccac52cf1b3867f42dff6ed9de0ab85
vip7009pro <hung1893@gmail.com>
2022-11-29 16:52:42 +0700

    update

---
+++ lib/LoginPage.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void _showToast(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(SnackBar(
+      content: const Text('Đăng nhập thành công !'),
+      action:
+          SnackBarAction(label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+    ));
+  }
+
+  Widget build(BuildContext context) {
+    final logo = Container(
+        child: Image.asset(
+      'assets/images/cmslogo.jpg',
+    ));
+
+    final username = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Tên đăng nhập.....',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Mật khẩu',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final loginButton = TextButton(
+      child: const Text('Đăng nhập'),
+      style: TextButton.styleFrom(
+          primary: Color.fromARGB(255, 255, 255, 255),
+          backgroundColor: Color.fromARGB(255, 70, 139, 241)),
+      onPressed: () {
+        _showToast(context);
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const LoginPage2()));
+      },
+    );
+    final forgotLabel = TextButton(
+      child: const Text('Quên mật khẩu?'),
+      onPressed: () {
+        _showToast(context);
+      },
+    );
+
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+              child: ListView(
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                children: [
+                  logo,
+                  const SizedBox(height: 45.0),
+                  username,
+                  const SizedBox(height: 10.0),
+                  password,
+                  const SizedBox(height: 15.0),
+                  loginButton,
+                  forgotLabel,
+                ],
+              ),
+            )));
+  }
+}
+
+class LoginPage2 extends StatefulWidget {
+  const LoginPage2({super.key});
+  @override
+  _LoginPageState2 createState() => new _LoginPageState2();
+}
+
+class _LoginPageState2 extends State<LoginPage2> {
+  @override
+  void _showToast(BuildContext context) {
+    final scaffold = ScaffoldMessenger.of(context);
+    scaffold.showSnackBar(SnackBar(
+      content: const Text('Đăng nhập thành công !'),
+      action:
+          SnackBarAction(label: 'OK', onPressed: scaffold.hideCurrentSnackBar),
+    ));
+  }
+
+  Widget build(BuildContext context) {
+    final logo = Container(
+        child: Image.asset(
+      'assets/images/cmslogo.jpg',
+    ));
+
+    final username = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      initialValue: '',
+      decoration: InputDecoration(
+        hintText: 'Tên đăng nhập.....',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Mật khẩu',
+        contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final loginButton = TextButton(
+      child: const Text('Đăng xuất'),
+      style: TextButton.styleFrom(
+          primary: Color.fromARGB(255, 255, 255, 255),
+          backgroundColor: Color.fromARGB(255, 70, 139, 241)),
+      onPressed: () {
+        _showToast(context);
+        Navigator.pop(context);
+      },
+    );
+    final forgotLabel = TextButton(
+      child: const Text('Quên mật khẩu?'),
+      onPressed: () {
+        _showToast(context);
+      },
+    );
+
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Main'),
+            ),
+            backgroundColor: Colors.white,
+            body: Center(
+              child: ListView(
+                shrinkWrap: true,
+                padding: EdgeInsets.only(left: 24.0, right: 24.0),
+                children: [
+                  SizedBox(height: 45.0),
+                  username,
+                  SizedBox(height: 10.0),
+                  password,
+                  SizedBox(height: 15.0),
+                  loginButton,
+                  forgotLabel,
+                ],
+              ),
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'LoginPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,105 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    final wordpair = WordPair.random();
     return const MaterialApp(
-      title: 'Welcome to HNPage',
-      home: RandomWords(),
+      debugShowCheckedModeBanner: false,
+      title: 'CMS VINA APP',
+      home: LoginPage(),
     );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Save Likes'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Ten auto'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.list),
-              onPressed: _pushSaved,
-              tooltip: 'Save likes',
-            ),
-          ],
-        ),
-        body: ListView.builder(
-          padding: const EdgeInsets.all(10.6),
-          itemBuilder: ((context, i) {
-            if (i.isOdd) return const Divider();
-            final index = i ~/ 2;
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(generateWordPairs().take(10));
-            }
-            final alreadySaved = _saved.contains(_suggestions[index]);
-            return ListTile(
-              title: Text(
-                _suggestions[index].asPascalCase,
-                style: _biggerFont,
-              ),
-              trailing: Icon(
-                alreadySaved ? Icons.favorite : Icons.favorite_border,
-                color: alreadySaved ? Color.fromARGB(255, 54, 244, 101) : null,
-                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-              ),
-              onTap: () {
-                // NEW from here ...
-                setState(() {
-                  if (alreadySaved) {
-                    _saved.remove(_suggestions[index]);
-                  } else {
-                    _saved.add(_suggestions[index]);
-                  }
-                });
-              },
-              onLongPress: () {
-                print('nguyen van hung longpress');
-              },
-            );
-          }),
-        ));
   }
 }
