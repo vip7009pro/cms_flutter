964bc626f78c8359b4866d92bb517ee756ed1dcc
vip7009pro <hung1893@gmail.com>
2023-11-30 20:12:02 +0700

    update home widget

--- lib/pages/HomeWidget.dart
+++ lib/pages/HomeWidget.dart
@@ -129,70 +129,144 @@ class _HomeWidgetState extends State<HomeWidget> {
         children: [
           Column(
             children: [
-              Row(
-                children: [
-                  avatar,
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(8.0),
-                          decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),                           
-                            gradient: LinearGradient(
-                                colors: [
-                                  Color.fromARGB(255, 133, 196, 248),
-                                  Color.fromARGB(255, 118, 173, 218),
-                                ],
-                                begin: FractionalOffset(0.0, 0.0),
-                                end: FractionalOffset(1.0, 0.0),
-                                stops: [0.0, 1.0],
-                                tileMode: TileMode.clamp),
-                              boxShadow: [
-                                 BoxShadow(
-                                color: Colors.black.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 10,
-                                offset: Offset(0, 3),
-                              ),
-                              ]
-                          ),
-                          child: Text("$_minTime")),
-                      Text("$_maxTime"),                      
-                    ],
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Thông tin nhân viên",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(                  
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    gradient: const LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 237, 248, 243),
+                          Color.fromARGB(255, 118, 218, 156),
+                        ],
+                        begin: FractionalOffset(1.0, 0.5),
+                        end: FractionalOffset(0.0, 1.0),
+                        stops: [0.0, 1.0],
+                        tileMode: TileMode.clamp),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 3),
+                      ),
+                    ]),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    avatar,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text("Chấm công hôm nay", style: TextStyle(fontWeight: FontWeight.bold),),      
+                        const SizedBox(height: 8.0,),     
+                        Row(                        
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                              margin: const EdgeInsets.only(right: 8.0),
+                                decoration:  BoxDecoration(
+                                  borderRadius: const BorderRadius.all(Radius.circular(10)),                           
+                                  gradient: const LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(255, 133, 196, 248),
+                                        Color.fromARGB(255, 118, 173, 218),
+                                      ],
+                                      begin: FractionalOffset(0.0, 0.0),
+                                      end: FractionalOffset(1.0, 0.0),
+                                      stops: [0.0, 1.0],
+                                      tileMode: TileMode.clamp),
+                                    boxShadow: [
+                                       BoxShadow(
+                                      color: Colors.black.withOpacity(0.2),
+                                      spreadRadius: 5,
+                                      blurRadius: 10,
+                                      offset: const Offset(0, 3),
+                                    ),
+                                    ]
+                                ),
+                                child: Text("$_minTime")),
+                            Container(
+                              padding: const EdgeInsets.all(8.0),
+                                decoration:  BoxDecoration(
+                                  borderRadius: const BorderRadius.all(Radius.circular(10)),                           
+                                  gradient: const LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(255, 133, 196, 248),
+                                        Color.fromARGB(255, 118, 173, 218),
+                                      ],
+                                      begin: FractionalOffset(0.0, 0.0),
+                                      end: FractionalOffset(1.0, 0.0),
+                                      stops: [0.0, 1.0],
+                                      tileMode: TileMode.clamp),
+                                    boxShadow: [
+                                       BoxShadow(
+                                      color: Colors.black.withOpacity(0.2),
+                                      spreadRadius: 5,
+                                      blurRadius: 10,
+                                      offset: const Offset(0, 3),
+                                    ),
+                                    ]
+                                ),
+                                child: Text("$_maxTime")),
+                          ],
+                        ),                                           
+                      ],
                     ),
-                  ),
-                  Text(
-                      "1. Họ và tên: ${_myUserData.mIDLASTNAME} ${_myUserData.fIRSTNAME}"),
-                  Text("2. Mã nhân sự: ${_myUserData.cMSID}"),
-                  Text("3. Mã ERP: ${_myUserData.eMPLNO}"),
-                  Text(
-                      "4. DOB: ${GlobalFunction.MyDate('dd-MM-yyyy', _myUserData.dOB!)}"),
-                  Text("5. Quê quán: ${_myUserData.hOMETOWN}"),
-                  Text(
-                      "6. Địa chỉ: ${_myUserData.aDDVILLAGE}-${_myUserData.aDDCOMMUNE}-${_myUserData.aDDDISTRICT}-${_myUserData.aDDPROVINCE}"),
-                  Text("7. Bộ phận chính: ${_myUserData.mAINDEPTNAME}"),
-                  Text("8. Bộ phận phụ: ${_myUserData.sUBDEPTNAME}"),
-                  Text("9. Vị trí làm việc: ${_myUserData.wORKPOSITIONNAME}"),
-                  Text("10. Nhóm điểm danh: ${_myUserData.aTTGROUPCODE}"),
-                  Text("11. Chức vụ: ${_myUserData.jOBNAME}"),
-                ],
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 8.0),
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(                  
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    gradient: const LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 237, 248, 243),
+                          Color.fromARGB(255, 118, 218, 156),
+                        ],
+                        begin: FractionalOffset(1.0, 0.5),
+                        end: FractionalOffset(0.0, 1.0),
+                        stops: [0.0, 1.0],
+                        tileMode: TileMode.clamp),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 3),
+                      ),
+                    ]),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Thông tin nhân viên",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                        "1. Họ và tên: ${_myUserData.mIDLASTNAME} ${_myUserData.fIRSTNAME}"),
+                    Text("2. Mã nhân sự: ${_myUserData.cMSID}"),
+                    Text("3. Mã ERP: ${_myUserData.eMPLNO}"),
+                    Text(
+                        "4. DOB: ${GlobalFunction.MyDate('dd-MM-yyyy', _myUserData.dOB!)}"),
+                    Text("5. Quê quán: ${_myUserData.hOMETOWN}"),
+                    Text(
+                        "6. Địa chỉ: ${_myUserData.aDDVILLAGE}-${_myUserData.aDDCOMMUNE}-${_myUserData.aDDDISTRICT}-${_myUserData.aDDPROVINCE}"),
+                    Text("7. Bộ phận chính: ${_myUserData.mAINDEPTNAME}"),
+                    Text("8. Bộ phận phụ: ${_myUserData.sUBDEPTNAME}"),
+                    Text("9. Vị trí làm việc: ${_myUserData.wORKPOSITIONNAME}"),
+                    Text("10. Nhóm điểm danh: ${_myUserData.aTTGROUPCODE}"),
+                    Text("11. Chức vụ: ${_myUserData.jOBNAME}"),
+                  ],
+                ),
               ),
             ],
-          ),
-          Text("Đây là Home widget"),
-          Text("Đây là Home widget"),
+          ),          
         ],
       ),
     );
