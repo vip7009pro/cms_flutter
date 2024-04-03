2487c046092a1aa8ed335ee237a74429e2581d1e
vip7009pro <hung1893@gmail.com>
2024-04-03 14:04:35 +0700

    upver

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -65,7 +65,7 @@ Future<void> tryOtaUpdate() async {
   final logo = Image.asset('assets/images/cmslogo.jpg', width: 120, fit: BoxFit.cover);
  
 
-  int mobileVer =5; 
+  int mobileVer =6; 
   late Timer _timer;
 
  Future<bool> _checkMobileVer() async {
