3ff31b98fb3ee6dc3edc2a463c97c7a2796e7269
vip7009pro <hung1893@gmail.com>
2024-04-03 13:30:09 +0700

    update flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,26 @@ import 'package:cms_flutter/controller/GetXController.dart';
 import 'package:cms_flutter/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   // ignore: library_private_types_in_public_api
   _MyAppState createState() => _MyAppState();
 }
-
 class _MyAppState extends State<MyApp> {
   final GlobalController c = Get.put(GlobalController());
   @override
-  void initState() { 
+  void initState() {
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'CMS VINA APP',          
+      title: 'CMS VINA APP',
       home: LoginPage(),
     );
   }
