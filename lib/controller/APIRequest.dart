0d2eb0b14ad96aa9e1e39f894e005e238e13f234
vip7009pro <hung1893@gmail.com>
2024-04-23 16:18:30 +0700

    update upload file

--- lib/controller/APIRequest.dart
+++ lib/controller/APIRequest.dart
@@ -4,10 +4,10 @@ import 'package:cms_flutter/controller/LocalDataAccess.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
-
 // ignore: camel_case_types
 class API_Request {
-  static Future<Map<String,dynamic>> api_query(String command, Map<String, dynamic> data) async {
+  static Future<Map<String, dynamic>> api_query(
+      String command, Map<String, dynamic> data) async {
     String url = '';
     url = await LocalDataAccess.getVariable('serverIP');
     if (url == '' || url == 'MAIN_SERVER') {
@@ -28,26 +28,26 @@ class API_Request {
         }));
     var cookieJar = CookieJar();
     dio.interceptors.add(CookieManager(cookieJar));
-    String savedToken = await LocalDataAccess.getVariable("token");    
+    String savedToken = await LocalDataAccess.getVariable("token");
     data['token_string'] = savedToken;
-    final body = {'command': command, 'DATA': data};    
+    final body = {'command': command, 'DATA': data};
     try {
       final response = await dio.post(url, data: jsonEncode(body));
       if (response.statusCode == 200) {
         return response.data;
       } else if (response.statusCode == 404) {
-        return {'tk_status': 'NG', 'message': 'Không tìm thấy dữ liệu'};        
+        return {'tk_status': 'NG', 'message': 'Không tìm thấy dữ liệu'};
       } else {
-        return {'tk_status': 'NG', 'message': 'Kết nối có vấn đề'};        
+        return {'tk_status': 'NG', 'message': 'Kết nối có vấn đề'};
       }
-    } on DioException catch (e) {      
+    } on DioException catch (e) {
       return {'tk_status': 'NG', 'message': '$e'};
-    } catch (e) {     
+    } catch (e) {
       return {'tk_status': 'NG', 'message': '$e'};
     }
   }
-
-  static Future<Map<String,dynamic>> api_upload_query(String filepath, String filename, String uploadfoldername) async {
+  static Future<Map<String, dynamic>> api_upload_query(
+      String filepath, String filename, String uploadfoldername) async {
     String url = '';
     url = await LocalDataAccess.getVariable('serverIP');
     if (url == '' || url == 'MAIN_SERVER') {
@@ -67,41 +67,41 @@ class API_Request {
           return true;
         }));
     var cookieJar = CookieJar();
-    dio.interceptors.add(CookieManager(cookieJar));   
-    String savedToken = await LocalDataAccess.getVariable("token");    
-     FormData body = FormData.fromMap({
-        'uploadedfile': await MultipartFile.fromFile(filepath, filename: filename),
-        'filename': filename,
-        'uploadfoldername':uploadfoldername,
-        'token_string': savedToken,        
-      }); 
+    dio.interceptors.add(CookieManager(cookieJar));
+    String savedToken = await LocalDataAccess.getVariable("token");
+    FormData body = FormData.fromMap({
+      'uploadedfile':
+          await MultipartFile.fromFile(filepath, filename: filename),
+      'filename': filename,
+      'uploadfoldername': uploadfoldername,
+      'token_string': savedToken,
+    });
     print(body.fields);
     final body2 = {
-        'uploadedfile': '123456789',
-        'filename': filename,
-        'uploadfoldername':uploadfoldername,
-        'token_string': savedToken,        
-        };  
+      'uploadedfile': '123456789',
+      'filename': filename,
+      'uploadfoldername': uploadfoldername,
+      'token_string': savedToken,
+    };
     try {
-      final response = await dio.post(url, data: jsonEncode(body2),  onSendProgress: (int sent, int total) {
-         print('Uploading: $sent/$total');          
+      final response = await dio.post(
+        url,
+        data: jsonEncode(body2),
+        onSendProgress: (int sent, int total) {
+          print('Uploading: $sent/$total');
         },
-        );
+      );
       if (response.statusCode == 200) {
         return response.data;
       } else if (response.statusCode == 404) {
-        return {'tk_status': 'NG', 'message': 'Không tìm thấy dữ liệu'};        
+        return {'tk_status': 'NG', 'message': 'Không tìm thấy dữ liệu'};
       } else {
-        return {'tk_status': 'NG', 'message': 'Kết nối có vấn đề'};        
+        return {'tk_status': 'NG', 'message': 'Kết nối có vấn đề'};
       }
-    } on DioException catch (e) {      
+    } on DioException catch (e) {
       return {'tk_status': 'NG', 'message': '$e'};
-    } catch (e) {     
+    } catch (e) {
       return {'tk_status': 'NG', 'message': '$e'};
     }
   }
-
-
 }
-  
-
